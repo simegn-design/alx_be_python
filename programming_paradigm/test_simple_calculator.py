195c79e86968af6ca7c1d6ba96fd5840468b6610
Simegneww <usesimegn@gmail.com>
2025-06-13 16:05:51 +0300

    Fixed unit tests for SimpleCalculator class

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -2,19 +2,30 @@ import unittest
 from simple_calculator import SimpleCalculator
 
 class TestSimpleCalculator(unittest.TestCase):
+    """Test cases for SimpleCalculator class."""
+    
     def setUp(self):
+        """Create a calculator instance for all tests."""
         self.calc = SimpleCalculator()
     
-    def test_add(self):
+    def test_addition(self):
+        """Test addition operation."""
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(0, 0), 0)
     
-    def test_subtract(self):
+    def test_subtraction(self):
+        """Test subtraction operation."""
         self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(10, -2), 12)
     
-    def test_multiply(self):
+    def test_multiplication(self):
+        """Test multiplication operation."""
         self.assertEqual(self.calc.multiply(3, 4), 12)
+        self.assertEqual(self.calc.multiply(7, 0), 0)
     
-    def test_divide(self):
+    def test_division(self):
+        """Test division operation."""
         self.assertEqual(self.calc.divide(6, 3), 2)
+        self.assertEqual(self.calc.divide(5, 2), 2.5)
         self.assertIsNone(self.calc.divide(5, 0))
